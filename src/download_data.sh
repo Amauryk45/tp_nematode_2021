d4766042f2541dcaa711b2c657117edc9030408e
Amauryk45 <jojofisho@gmail.com>
2021-11-17 13:37:25 +0000

    Add import SRR data

---
+++ src/download_data.sh
@@ -0,0 +1,13 @@
+DATA="SRR5564855
+SRR5564856
+SRR5564857
+SRR5564858
+SRR5564859
+SRR5564860
+"
+cd ~/mydatalocal/tp_nematode_2021/data/
+for SRR in $DATA
+do
+   fastq-dump --split-files --gzip $SRR
+done
+
