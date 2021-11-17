30a499f67e267e425f17e487087177c221cdefee
Amauryk45 <jojofisho@gmail.com>
2021-11-17 14:02:49 +0000

    test version for download script

--- src/download_data.sh
+++ src/download_data.sh
@@ -1,3 +1,10 @@
+#! /bin/bash
+
+#This code downloads SRR data from WT and alg-5 tm mutant nematode
+#Paired data are converted in zipped fastq files 
+
+
+#Defines variable with all 6 SRR samples (3 WT + 3 alg-5 tm)
 DATA="SRR5564855
 SRR5564856
 SRR5564857
@@ -5,10 +12,10 @@ SRR5564858
 SRR5564859
 SRR5564860
 "
+
 cd ~/mydatalocal/tp_nematode_2021/data/
+
 for SRR in $DATA
 do
-   fastq-dump --split-files --gzip $SRR
+   fastq-dump -X 5 --split-3 --gzip $SRR
 done
-
-
