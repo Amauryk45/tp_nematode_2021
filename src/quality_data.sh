a1624d893f227436fb2ddbb3a1720dc7286e9284
Amauryk45 <jojofisho@gmail.com>
2021-11-17 16:16:17 +0000

    add mkdir results trimming

--- src/quality_data.sh
+++ src/quality_data.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+#This code use fastqc and multiqc functions to give informations on the quality of the data imported
 
 cd ~/mydatalocal/tp_nematode_2021/
 mkdir results/fastqc_raw
 mkdir results/multiqc_raw
+mkdir results/trimmed_data
 
 fastqc -o results/fastqc_raw data/*.fastq.gz
 multiqc -o results/multiqc_raw results/fastqc_raw
