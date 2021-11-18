28f46d0b972b64152a5713cec31a07882a118e2d
Amauryk45 <jojofisho@gmail.com>
2021-11-18 10:38:18 +0000

    Correct destination file

--- src/download_cdna.sh
+++ src/download_cdna.sh
@@ -4,7 +4,5 @@
 #This code downloads cDNA data from C.elegans from http://ftp.ensembl.org/pub/release-104/fasta/caenorhabditis_elegans/cdna/
 cd ~/mydatalocal/tp_nematode_2021/
 
-wget -P data/Celegans_cDNA/ \
-http://ftp.ensembl.org/pub/release-104/fasta/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz \
-gunzip -k data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz 
-head data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa
+wget -P data/Celegans_cDNA/ http://ftp.ensembl.org/pub/release-104/fasta/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz
+gunzip -k ~/mydatalocal/tp_nematode_2021/data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz 
