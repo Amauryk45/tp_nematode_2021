73dfea702294a15206204d3d24939131a150cf25
Amauryk45 <jojofisho@gmail.com>
2021-11-18 11:01:41 +0000

    Correct destination files

--- src/align_and_quantify.sh
+++ src/align_and_quantify.sh
@@ -2,5 +2,35 @@
 
 #This code align RNAseq data to C.elegans reference transcirptome using salmon
 
-salmon index -t ~/mydatalocal/tp_nematode_2021/data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz \  
--i ~/mydatalocal/tp_nematode_2021/results/alignment_data/Caenorhabditis_elegans.WBcel235_idx
+Data="SRR5564855
+SRR5564856
+SRR5564857
+SRR5564858
+SRR5564859
+SRR5564860
+"
+
+#Creation of salmon index of C.elegans transcriptome
+salmon index -t ~/mydatalocal/tp_nematode_2021/data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa -i ~/mydatalocal/tp_nematode_2021/results/alignment_data/Caenorhabditis_elegans.WBcel235_idx
+
+for Srr in $Data
+ do
+ samp=`basename ${Srr}`
+ echo "Processing sample ${samp}"
+ salmon quant -i ~/mydatalocal/tp_nematode_2021/results/alignment_data/Caenorhabditis_elegans.WBcel235_idx -l A \
+         -1 data/samples_data/${Srr}_1.fastq.gz \
+         -2 data/samples_data/${Srr}_2.fastq.gz \
+         -p 4 --validateMappings -o quants/${samp}_quant
+done 
+
+
+
+for fn in data/DRR0161{25..40};
+do
+samp=`basename ${Srr}`
+echo "Processing sample ${samp}"
+salmon quant -i athal_index -l A \
+         -1 ${fn}/${samp}_1.fastq.gz \
+         -2 ${fn}/${samp}_2.fastq.gz \
+         -p 8 --validateMappings -o quants/${samp}_quant
+done 
