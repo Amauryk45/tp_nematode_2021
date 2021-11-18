8e23d018fc20ed724604f23c034b445192b195a5
Amauryk45 <jojofisho@gmail.com>
2021-11-18 14:46:01 +0000

    Correct files destination

--- src/align_and_quantify.sh
+++ src/align_and_quantify.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#This code align RNAseq data to C.elegans reference transcirptome using salmon
+#This code aligns RNAseq data to the C.elegans reference transcirptome using salmon
 
 Data="SRR5564855
 SRR5564856
@@ -9,28 +9,25 @@ SRR5564858
 SRR5564859
 SRR5564860
 "
+cd ~/mydatalocal/tp_nematode_2021/
 
 #Creation of salmon index of C.elegans transcriptome
-salmon index -t ~/mydatalocal/tp_nematode_2021/data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa -i ~/mydatalocal/tp_nematode_2021/results/alignment_data/Caenorhabditis_elegans.WBcel235_idx
+salmon index -t data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa \
+-i results/alignment_data/Caenorhabditis_elegans.WBcel235_idx
+
 
 for Srr in $Data
  do
- samp=`basename ${Srr}`
- echo "Processing sample ${samp}"
- salmon quant -i ~/mydatalocal/tp_nematode_2021/results/alignment_data/Caenorhabditis_elegans.WBcel235_idx -l A \
-         -1 data/samples_data/${Srr}_1.fastq.gz \
-         -2 data/samples_data/${Srr}_2.fastq.gz \
-         -p 4 --validateMappings -o quants/${samp}_quant
+ echo "Processing sample ${Srr}"
+ salmon quant -i results/alignment_data/Caenorhabditis_elegans.WBcel235_idx -l A \
+         -1 results/trimmed_data/${Srr}_1_output_paired.fastq.gz \
+         -2 results/trimmed_data/${Srr}_2_output_paired.fastq.gz \
+         -p 4 --validateMappings -o results/alignment_data/quants/${Srr}_quant
 done 
 
 
-
-for fn in data/DRR0161{25..40};
-do
-samp=`basename ${Srr}`
-echo "Processing sample ${samp}"
-salmon quant -i athal_index -l A \
-         -1 ${fn}/${samp}_1.fastq.gz \
-         -2 ${fn}/${samp}_2.fastq.gz \
-         -p 8 --validateMappings -o quants/${samp}_quant
-done 
+#for fn in *;
+#do
+#samp=`basename ${fn}`
+#echo "${samp}"
+#done
