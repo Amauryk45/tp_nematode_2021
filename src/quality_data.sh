34d044c14d8a264bebec25412340138d85727243
Amauryk45 <jojofisho@gmail.com>
2021-11-18 10:38:06 +0000

    Correct destination file

--- src/quality_data.sh
+++ src/quality_data.sh
@@ -3,11 +3,6 @@
 #This code use fastqc and multiqc functions to give informations on the quality of the data imported
 
 cd ~/mydatalocal/tp_nematode_2021/
-mkdir results/fastqc_raw
-mkdir results/multiqc_raw
-mkdir results/trimmed_data
-mkdir data/Celegans_cDNA
 
-
-fastqc -o results/fastqc_raw data/*.fastq.gz
+fastqc -o results/fastqc_raw data/samples_data/*.fastq.gz
 multiqc -o results/multiqc_raw results/fastqc_raw
