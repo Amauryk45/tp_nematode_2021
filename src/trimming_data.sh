31b96956f95142acd7ec102c57cec6f893370ab2
Amauryk45 <jojofisho@gmail.com>
2021-11-18 09:19:31 +0000

    Correct trimmering script

--- src/trimming_data.sh
+++ src/trimming_data.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
 #This script uses trimmomatic to filter and trimme RNAseq data from fastq zipped files
-sudo apt-get update -y 
-sudo apt-get install -y trimmomatic 
- 
+#Variable data with SRA numbers of samples
 Data="SRR5564855
 SRR5564856
 SRR5564857
@@ -11,22 +9,29 @@ SRR5564858
 SRR5564859
 SRR5564860
 "
-
+#defines global path
 cd ~/mydatalocal/tp_nematode_2021/
 
 
-#Drop if read length < 75 nucleotides
-for Srr in Data
+for Srr in $Data
  do
-trimmomatic \
-  PE \
+java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar \
+  PE \ 
+  #pair-end
   data_test/${Srr}_1.fastq.gz \
   data_test/${Srr}_2.fastq.gz \
+  #both input files in /data_test
   results/trimmed_data/${Srr}_1_output_paired.fastq.gz \
   results/trimmed_data/${Srr}_1_output_unpaired.fastq.gz \
   results/trimmed_data/${Srr}_2_output_paired.fastq.gz \
   results/trimmed_data/${Srr}_2_output_unpaired.fastq.gz \
+  #four output files (paired/unpaired) in /results/trimmed_data (mkdir)
   -threads 4 \
-  SLIDINGWINDOW:4:28 MINLEN:75
+  ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:25 TRAILING:25 SLIDINGWINDOW:4:28 MINLEN:75
+  #Illuminaclip: remove adapters in TruSeq3 file (specific for Hiseq)
+  #Slidingwindow: Perform a sliding window trimming, cutting once the average quality within the window falls below a threshold. (every 4 if mean quality < 28)
+  #Leading: Cut bases off the start of a read, if below a threshold quality (25)
+  #Trailing: Cut bases off the end of a read, if below a threshold quality (25)
+  #Minlen:  Drop the read if it is below a specified length (75)
  done
  
