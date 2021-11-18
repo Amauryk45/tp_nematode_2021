f856592d4341b7bbb1e48b134d4ca480b761356d
Amauryk45 <jojofisho@gmail.com>
2021-11-18 16:07:31 +0000

    Correct salmon quant syntax

--- src/align_and_quantify.sh
+++ src/align_and_quantify.sh
@@ -6,22 +6,19 @@ cd ~/mydatalocal/tp_nematode_2021/
 Data=$(ls data/samples_data/ | sed 's/_[0-9].fastq.gz/ /g'| uniq)
 
 #Creation of salmon index of C.elegans transcriptome
-salmon index -t data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa \
--i results/alignment_data/Caenorhabditis_elegans.WBcel235_idx
+#salmon index -t data/Celegans_cDNA/Caenorhabditis_elegans.WBcel235.cdna.all.fa \
+#-i results/alignment_data/Caenorhabditis_elegans.WBcel235_idx
 
 
 for Srr in $Data
  do
  echo "Processing sample ${Srr}"
- salmon quant -i results/alignment_data/Caenorhabditis_elegans.WBcel235_idx -l A \
-         -1 results/trimmed_data/${Srr}_1_output_paired.fastq.gz \
-         -2 results/trimmed_data/${Srr}_2_output_paired.fastq.gz \
-         -p 4 --validateMappings -o results/alignment_data/quants/${Srr}_quant
+ salmon quant \
+ --index results/alignment_data/Caenorhabditis_elegans.WBcel235_idx \
+ -l A \
+ -1 results/trimmed_data/${Srr}_1_output_paired.fastq.gz \
+ -2 results/trimmed_data/${Srr}_2_output_paired.fastq.gz \
+ --threads 4 \
+ --validateMappings \
+ --output results/alignment_data/quants/${Srr}_quant
 done 
-
-
-#for fn in *;
-#do
-#samp=`basename ${fn}`
-#echo "${samp}"
-#done
