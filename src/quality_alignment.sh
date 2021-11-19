52a167c72e5991b4a86b1a603b9e13fb8d458c2b
Amauryk45 <jojofisho@gmail.com>
2021-11-19 09:48:14 +0000

    chosen*

--- src/quality_alignment.sh
+++ src/quality_alignment.sh
@@ -8,7 +8,7 @@ echo input_dir $input_dir
 echo suffix_out $suffix_out
 
 cd ~/mydatalocal/tp_nematode_2021/
-#make output directory in results with the choosen suffix
+#Make output directory in results with the choosen suffix
 mkdir -p results/multiqc_${suffix_out}
 #Give output in this directory, and take in intput a given directory
 multiqc -o results/multiqc_${suffix_out} ${input_dir}
