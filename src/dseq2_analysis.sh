2e34a8feb9bdfa4d68e2480d474e293381512c0d
Amaury Korb <amaury.korb@ens-lyon.fr>
2021-12-14 15:32:46 +0000

    script r

---
+++ src/dseq2_analysis.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+#This script uses 
+
+cd ~/mydatalocal/tp_nematode_2021/
+
+library(tximport)
+paths=c("SRR5564861","SRR5564862","SRR5564863","SRR5564855","SRR5564856","SRR5564857")
+for i in paths {
+  i <- i+'_quant'
+  print(i)
+}
+fullpaths=file.path("data/results/alignment_data/quants", paths,"quant.sf")
+
+cts=tximport(fullpaths,'salmon',txOut=TRUE)
+dds <- DESeqDataSetFromTximport(countData =cts)
+dds <- DESeq(dds)
+resultsNames(dds)
+
+colData=data.frame(paths,c("Arg1","Arg1","Arg1","WT","WT","WT"))
+colnames(colData)=c("sample","treatment")
+dp=DESeqDataSetFromMatrix(round(data$counts),colData = colData,design=~treatment)
+results=DESeq(dp,test="Wald")
+
+plotMA(results,ylim=c(-12,12))
+
+tx2gn <- read.table("path/to/tx2gn.csv", h=T, sep=',', as.is=T)
+data <- tximport(...., tx2gene = tx2gn) # enlever l'option txOut si vous l'avez mise
