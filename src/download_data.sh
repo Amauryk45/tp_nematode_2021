4a1f7b0fdd1ee3d49072b4289d3f4ea8f3ceb2b1
Amaury Korb <amaury.korb@ens-lyon.fr>
2021-12-14 15:33:02 +0000

    Correct script filpath

--- src/download_data.sh
+++ src/download_data.sh
@@ -6,9 +6,15 @@
 
 #Defines variable with all 6 SRR samples (3 WT + 3 alg-5 tm)
 
-cd ~/mydatalocal/tp_nematode_2021/data/
-Data=$(ls data/samples_data/ | sed 's/_[0-9].fastq.gz/ /g'| uniq)
+cd ~/mydatalocal/tp_nematode_2021/data/samples_data
+###Data=$(ls samples_data/ | sed 's/_[0-9].fastq.gz/ /g'| uniq)
 
+Data="SRR5564858
+SRR5564859
+SRR5564860
+SRR5564855
+SRR5564856
+SRR5564857"
 for Srr in $Data
 do
    parallel-fastq-dump --split-3 --gzip -s $Srr --threads 6
