2bbfb1987eed5a4b5577e27003f89b9dedbb5c0e
Amaury Korb <amaury.korb@ens-lyon.fr>
2021-12-14 15:57:37 +0000

    DSeq2 analysis in the R script

--- src/dseq r.R
+++ src/dseq r.R
@@ -1,7 +1,38 @@
-
+library(DESeq2)
 library(tximport)
-paths=c("SRR5564861","SRR5564862","SRR5564863","SRR5564855","SRR5564856","SRR5564857")
-fullpaths=file.path("data/results/alignment_data/quants", lapply(paths, paste0, "_quant"),"quant.sf")
+
+
+paths=c("SRR5564855","SRR5564856","SRR5564857","SRR5564858","SRR5564859","SRR5564860")
+paths_quant <-  unlist(lapply(paths, paste0, "_quant"))
+quant_files=file.path("results/alignment_data/quants", paths_quant,"quant.sf")
+
+
+tx2gn <- read.table('~/mydatalocal/tp_nematode_2021/data/tx2gn.csv', h=T, sep=',', as.is=T)
+
+txi_salmon <- tximport(quant_files,
+                       type = 'salmon', 
+                       tx2gene = tx2gn          
+)
+
+
+
+head(txi_salmon$counts)
+
+
+
+ColData <- data.frame(paths, rep(c("wt", "alg-5(ram2)"), e=3))
+colnames(ColData) <- c("samples", "treatment")
+
+dds <- DESeqDataSetFromMatrix(round(txi_salmon$counts), colData = ColData , design = ~ treatment)
+
+quantif <- DESeq(dds, test = "Wald")
+res <- results(quantif) #extracts log2(fold_change) and form DESeq output
+res
+
+plot(x = res$log2FoldChange, y = -log10(res$pvalue), ylim = c(0,4)) #raw plot
+plotMA(res, ylim = c(-12,12))  #nice plot
+
+plot(res$padj)
 
 
 
